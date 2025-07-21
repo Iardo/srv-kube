4ee1a33897c9e8f571cb65c80f37aca2cef46e56
Ivan Ramos <iardoru@gmail.com>
2025-07-21 15:46:09 -0500

    FEAT: Update service aria2-pro

--- _/aria2-pro/stops.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/bash
-set -e
-
-docker-compose stop
-sudo truncate -s 0 $(docker inspect --format='{{.LogPath}}' aria2-pro)
-sudo truncate -s 0 $(docker inspect --format='{{.LogPath}}' aria2-pro-ng)
-docker-compose down
