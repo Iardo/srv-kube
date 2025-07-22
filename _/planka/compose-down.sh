cdb7a5ed961e1c98cabf015cff6237cb228e2cb0
Ivan Ramos <iardoru@gmail.com>
2025-07-22 00:07:34 -0500

    FEAT: Update service planka

--- _/planka/compose-down.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/bash
-set -e
-set -o pipefail
-
-docker-compose stop
-sudo truncate -s 0 $(docker inspect --format='{{.LogPath}}' planka-web)
-sudo truncate -s 0 $(docker inspect --format='{{.LogPath}}' planka-database)
-docker-compose down -v
