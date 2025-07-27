3896bb68da27f7007d07f8a173bf4af207bcedb5
Ivan Ramos <iardoru@gmail.com>
2025-07-27 02:22:02 -0500

    FIX: Update backup filename format

--- serv/open-project/code/script/backup.sh
+++ serv/open-project/code/script/backup.sh
@@ -4,8 +4,8 @@ set -o pipefail
 
 source /.env
 
-timestamp=$(date +"%Y%m%d-%H%M%S")
-file_sql="${timestamp}.sql"
+timestamp=$(date + "%Y.%m%.d-%H.%M.%S")
+file_sql="${timestamp}-backup.sql"
 file_pgdata="${timestamp}-pgdata.tar.gz"
 file_opdata="${timestamp}-opdata.tar.gz"
 
