797601290b33193c4be98ad0a71b9f2a8d48cc6d
Iván Ramos <iardoru@gmail.com>
2025-08-07 13:24:58 -0500

    FIX: Change text in HOST_SELECTION_TEXT to a more general one, so does not print "install" for all scripts

--- source/globals/text.py
+++ source/globals/text.py
@@ -2,7 +2,7 @@ from source.globals.color import Color
 
 class Text:
     HOST_SELECTION_TITLE: str = f'{Color.text["underline"]}Host Selection:{Color.text["reset"]}'
-    HOST_SELECTION_TEXT: str = f'Which host do you want to install? (Input the number): '
+    HOST_SELECTION_TEXT: str = f'Which host do you want to select? (Input the number): '
     SCRIPT_DESCRIPTION_INIT: str = f'Initialize a host configuration.'
     SCRIPT_DESCRIPTION_START: str = f'Starts all the host services leveraging docker-compose.'
     SCRIPT_DESCRIPTION_STOPS: str = f'Stops all the host services'
