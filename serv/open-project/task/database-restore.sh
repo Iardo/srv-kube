2bf2e878bec538564af0d18bbb6a2415198102d7
iardo <iardo@icelemonade.com>
2025-08-07 17:29:10 -0500

    FEAT: Add finish message on database-restore.sh for service open-project

--- serv/open-project/task/database-restore.sh
+++ serv/open-project/task/database-restore.sh
@@ -5,6 +5,8 @@ set -o pipefail
 quiet="> /dev/null 2>&1"
 
 fullpath=$(dirname "$0")
+color_green='\033[0;32m'
+color_reset='\033[0m'
 database_path=$fullpath/../data/backup
 database_file=
 
@@ -24,3 +26,19 @@ CREATE SCHEMA public;\
 echo "Restoring: $database_file.sql ..."
 docker exec -it open-project-database sh -c "PGPASSWORD=$POSTGRESQL_PASS psql --quiet -h localhost -U $POSTGRESQL_USER -d $POSTGRESQL_NAME -c $cmd_restore $quiet"
 docker exec -it open-project-database sh -c "PGPASSWORD=$POSTGRESQL_PASS psql --quiet -h localhost -U $POSTGRESQL_USER -d $POSTGRESQL_NAME -f /backups/$database_file.sql -L /logs/$database_file-restore.log $quiet"
+
+message=$(cat << EOF
+OPEN-PROJECT: Database Restored
+----
+The service needs to be restarted
+Please run the next command:
+    ../../start.py
+
+EOF
+)
+
+echo -e "\
+${color_green}\
+${message}\
+${color_reset}
+"
