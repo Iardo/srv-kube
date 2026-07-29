ef09c7db6b9be56b5d0fc9276685a716c7f63977
Iván Ramos <iardoru@gmail.com>
2026-07-28 23:16:39 -0500

    FEAT: Update outline | WIP

--- todo/outline/stops.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/bash
-set -e
-
-docker-compose stop
-sudo truncate -s 0 $(docker inspect --format='{{.LogPath}}' outline-web)
-sudo truncate -s 0 $(docker inspect --format='{{.LogPath}}' outline-postgres)
-sudo truncate -s 0 $(docker inspect --format='{{.LogPath}}' outline-redis)
-sudo truncate -s 0 $(docker inspect --format='{{.LogPath}}' outline-ssl)
-docker-compose down -v
